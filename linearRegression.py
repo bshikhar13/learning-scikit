5c0b09964babf2d8ada6759e5d102e56d0c2f99d
bshikhar13 <bshikhar13131313@gmail.com>
2015-06-29 18:52:37 +0530

    LinearRegression Example for multiple variables

--- linearRegression.py
+++ linearRegression.py
@@ -1,5 +1,5 @@
 from sklearn import linear_model
 clf = linear_model.LinearRegression()
-clf.fit ([[0, 0], [1, 1], [2, 2]], [0, 1, 2])
+clf.fit ([[59, 15,5,10], [-44, 8,15,-336], [7, 4,-6,15],[17, 42,-26,-15]] ,[13, 58, -66,45])
 print(clf)
 print(clf.coef_)
